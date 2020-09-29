f9e4eb69c39c75e384794abfefe93beced0946d5
Jessica cala <calajess@gmail.com>
2020-09-28 19:39:09 -0500

    first commit

---
+++ tallermongo.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+con = MongoClient()
+db = con["bd03"] # reemplace X por el número que corresponde a su usuario
+db.authenticate("bd03", "$bd03!", "admin")
+
+coll = db["Movies2"]
+
+doc = {}
+doc["Title"] = "Twelve Monkeys"
+doc["Year"] = 1995
+doc["Runtime"] = "129 min"
+doc["Directors"] = ["Terri Gilliam"]
+db.Movies2.insert_one(doc)
+
+for movie in db.Movies2.find():
+    print (movie)
