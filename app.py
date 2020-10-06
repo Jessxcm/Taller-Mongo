7acdedf26b38e86a7606b5deb9627dccac156494
Jessica cala <calajess@gmail.com>
2020-10-06 01:05:10 -0500

    cambios en los datos que ve el usuario al entrar en la app

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from connection import datetime,db
 #Entrada a la aplicacion
 
 #1. Los últimos 10 mensajes que el usuario ha publicado, en orden cronológico, empezando por el más reciente. Si son réplicas, indica el mensaje original y el usuario que lo publicó. Si son mensajes re-enviados, indica el usuario que publicó el mensaje original.
-db.UsersTweets.aggregate( 
+""" db.UsersTweets.aggregate( 
     [ 
         {
             $match : { creator : "rcator1@hubpages.com"}
@@ -33,7 +33,7 @@ db.UsersTweets.aggregate(
             }
         }
     ] 
-)
+) """
 
 #pymongo
 
@@ -72,21 +72,37 @@ for result in db.UsersTweets.aggregate(pipeline):
 
 #2. Los últimos 5 mensajes que han publicado cada uno de los usuarios a los que sigue, agrupados por el usuario que los publica. Los usuarios deben estar en orden alfabético y, los mensajes de cada usuario, en orden cronológico, empezando por el más reciente.
 
-
-listfollowing = []
-print ("usuarios a los que sigue")
-for user in db.Users2.find({ "username": "rsoaper3@adobe.com" }):
-    listfollowing=user['following']
-    #print (user['following'])
-
-#print (listfollowing[0])
-#print(listfollowing[0]['username'])
-
-for i in listfollowing:
-    print(listfollowing[i]['username'])
-
-
-
+for user in db.Users.find({ "username": "rcator1@hubpages.com" }):
+    for seguidos in user['following']:
+        cmd = seguidos['username']
+        pipeline=[
+            {
+                '$match' : { 'creator' : cmd}
+            },
+            {
+                '$sort' : { 'fecha' : -1 }
+            },
+            {
+                '$limit' : 5
+            },
+            {
+                '$project' : {
+                                'creator': 0,
+                                '_id':0
+                            }
+            }
+        ]
+        print ("====================================================================================================")
+        print ("mensajes de usuario seguido:  ", cmd)
+        print ("====================================================================================================")
+        for result in db.UsersTweets.aggregate(pipeline):
+            print ("tipo mensaje : ",result['type_message'])
+            print ("fecha : ",result['fecha'])
+            print ("contenido : ",result['content'])
+            if result['type_message']!="original":
+                print ("original de : ",result['from'])
+            print ("--------------------------------------")
+        print ("\n")
 
 
 
